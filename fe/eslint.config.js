54acb3e55c469ff374d6bcb0d1b7d892b4769c48
phanduythinh <phanduythinh1401@gmail.com>
2025-05-13 22:57:40 +0700

    UI quản lý khách hàng

--- fe/eslint.config.js
+++ fe/eslint.config.js
@@ -1,54 +1,65 @@
-// Updated by trungquandev.com's author on May 13 2023
-// Sample Eslint config for React project
-module.exports = {
-  env: { browser: true, es2020: true, node: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended'
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: [
-    'react',
-    'react-hooks',
-    'react-refresh'
-  ],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'react/prop-types': 0,
-    'react/display-name': 0,
+module.exports = [
+  require('@eslint/js').configs.recommended,
 
-    'no-restricted-imports': [
-      'error',
-      {
-        'patterns': ['@mui/*/*/*']
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    languageOptions: {
+      parser: require('@babel/eslint-parser'),
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ['@babel/preset-react']
+        }
+      },
+      globals: {
+        browser: true,
+        node: true
       }
-    ],
-
-    // common
-    'no-useless-catch': 0,
-    'no-console': 1,
-    'no-lonely-if': 1,
-    'no-extra-boolean-cast': 0,
-    'no-unused-vars': 1,
-    'no-trailing-spaces': 1,
-    'no-multi-spaces': 1,
-    'no-multiple-empty-lines': 1,
-    'space-before-blocks': ['error', 'always'],
-    'object-curly-spacing': [1, 'always'],
-    'indent': ['warn', 2],
-    'semi': [1, 'never'],
-    'quotes': ['error', 'single'],
-    'array-bracket-spacing': 1,
-    'linebreak-style': 0,
-    'no-unexpected-multiline': 'warn',
-    'keyword-spacing': 1,
-    'comma-dangle': 1,
-    'comma-spacing': 1,
-    'arrow-spacing': 1
+    },
+    settings: {
+      react: {
+        version: '18.2'
+      }
+    },
+    plugins: {
+      react: require('eslint-plugin-react'),
+      'react-hooks': require('eslint-plugin-react-hooks'),
+      'react-refresh': require('eslint-plugin-react-refresh')
+    },
+    rules: {
+      'react-refresh/only-export-components': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/prop-types': 0,
+      'react/display-name': 0,
+      'no-restricted-imports': [
+        'error',
+        {
+          'patterns': ['@mui/*/*/*']
+        }
+      ],
+      // common
+      'no-useless-catch': 0,
+      'no-console': 1,
+      'no-lonely-if': 1,
+      'no-unused-vars': 1,
+      'no-trailing-spaces': 1,
+      'no-multi-spaces': 1,
+      'no-multiple-empty-lines': 1,
+      'space-before-blocks': ['error', 'always'],
+      'object-curly-spacing': [1, 'always'],
+      'indent': ['warn', 2],
+      'semi': [1, 'never'],
+      'quotes': ['error', 'single'],
+      'array-bracket-spacing': 1,
+      'linebreak-style': 0,
+      'no-unexpected-multiline': 'warn',
+      'keyword-spacing': 1,
+      'comma-dangle': 1,
+      'comma-spacing': 1,
+      'arrow-spacing': 1
+    }
   }
-}
+];
--- fe/src/pages/ManageCustomers.jsx
+++ fe/src/pages/ManageCustomers.jsx
@@ -1,10 +1,92 @@
-import React from 'react'
+import * as React from 'react'
+import { styled } from '@mui/material/styles'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell, { tableCellClasses } from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
+import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: '#a8d8fb',
+    borderRight: '1px solid #e0e0e0'
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+    borderRight: '1px solid #e0e0e0'
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  }
+}));
+
+function createData(name, calories, fat, carbs, protein, a, b, c, d, e) {
+  return { name, calories, fat, carbs, protein, a, b, c, d, e };
+}
+
+const rows = [
+  createData('KH001', 'Nguyễn Văn A', 'Nam', '14/01/2003','0123345534', 'pdt1401@gmail.com', '0121232', 'co nhue 2, bac tu liem, ha noi', 'Nha A', '01'),
+  createData('KH002', 'Nguyễn Văn B', 'Nu', '14/01/2003','0123345534', 'pdt1401@gmail.com', '0121232', 'co nhue 2, bac tu liem, ha noi', 'Nha A', '01')
+];
 
 function ManageCustomers() {
   return (
-    <div>
-      Trang quan ly khach hang
-    </div>
+    <Box>
+      <h1>Quản lý khách hàng</h1>
+      <Box sx={{ display: 'flex', gap: 2 }}>
+        <Button variant="contained" fontSize='small'>Thêm khách hàng mới</Button>
+        <Button variant="contained" fontSize='small'>Xuất báo cáo</Button>
+      </Box>
+
+      <TableContainer component={Paper} sx={{ marginTop: '16px' }}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>ID khách hàng</StyledTableCell>
+              <StyledTableCell>Họ và tên</StyledTableCell>
+              <StyledTableCell>Giới tính</StyledTableCell>
+              <StyledTableCell>Ngày sinh</StyledTableCell>
+              <StyledTableCell>Số điện thoại</StyledTableCell>
+              <StyledTableCell>Email</StyledTableCell>
+              <StyledTableCell>CCCD</StyledTableCell>
+              <StyledTableCell>Địa chỉ thường trú</StyledTableCell>
+              <StyledTableCell>Nhà thuê</StyledTableCell>
+              <StyledTableCell>Phòng thuê</StyledTableCell>
+              <StyledTableCell>Chức năng</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <StyledTableRow key={row.name}>
+                <StyledTableCell component="th" scope="row"> {row.name}</StyledTableCell>
+                <StyledTableCell>{row.calories}</StyledTableCell>
+                <StyledTableCell>{row.fat}</StyledTableCell>
+                <StyledTableCell>{row.carbs}</StyledTableCell>
+                <StyledTableCell>{row.protein}</StyledTableCell>
+                <StyledTableCell>{row.a}</StyledTableCell>
+                <StyledTableCell>{row.b}</StyledTableCell>
+                <StyledTableCell>{row.c}</StyledTableCell>
+                <StyledTableCell>{row.d}</StyledTableCell>
+                <StyledTableCell>{row.e}</StyledTableCell>
+                <StyledTableCell>
+                  <Box sx={{ display: 'flex', gap: 1 }}>
+                  <Button variant="contained">Sửa</Button>
+                  <Button variant="contained">Xóa</Button>
+                  </Box>
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   )
 }
 
