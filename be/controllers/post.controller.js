f137defd2d6b2b6e3f58feb3eb3ea041d484a913
vhoangcnr <phantriviethoang@gmail.com>
2025-05-17 21:27:11 +0700

    set up

--- be/controllers/post.controller.js
+++
@@ -1,8 +0,0 @@
-function index(req, res) {
-  const posts = "Post list"
-  res.send(posts)
-}
-
-module.exports={
-  index: index
-}
--- be/routes/posts.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const postsController = require('../controllers/post.controller')
-
-const router = express.Router()
-
-router.get('/', postsController.index)
-
-module.exports = router
