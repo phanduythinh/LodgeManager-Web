60d1e3c840875873405e32abd0512483e65da2b0
phanduythinh <phanduythinh1401@gmail.com>
2025-05-20 02:56:58 +0700

    cn

--- fe/src/main.jsx
+++ fe/src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { ConfirmProvider } from "material-ui-confirm" //Cau hinh MUI dialog
+import { ConfirmProvider } from 'material-ui-confirm' //Cau hinh MUI dialog
+import theme from './theme.js'
+import { ThemeProvider } from '@mui/material/styles'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ConfirmProvider>
-      <App />
-    </ConfirmProvider>
-  </StrictMode>,
+    <ThemeProvider theme={theme}>
+      <ConfirmProvider>
+        <App />
+      </ConfirmProvider>
+    </ThemeProvider>
+  </StrictMode>
 )
