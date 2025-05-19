b9ebdf9d2408adf572f78693d0ec0dd6f6d3f18f
phanduythinh <phanduythinh1401@gmail.com>
2025-05-20 02:58:54 +0700

    UI quan ly phong

--- fe/src/apis/mock-data.js
+++ fe/src/apis/mock-data.js
@@ -1,11 +1,158 @@
-export const mockData = {
-  id: 'CH01',
-  TenNha: 'Ben Hou',
-  DiaChiNha: '85 phố Viên, Cổ Nhuế 2, Bắc Từ Liêm, Hà Nội',
-  TrangThai: 'Không',
-  Phong: [
-    {},
-    {},
-    {}
-  ]
-}
+export const ToaNhaData = [
+  {
+    id: 'CH-001',
+    TenNha: 'Ben Hou',
+    DiaChiNha: '85 phố Viên',
+    QuanHuyen: 'Bắc Từ Liêm',
+    TinhThanh: 'Hà Nội',
+    XaPhuong: 'Cổ Nhuế 2',
+    TrangThai: 'Hoạt động',
+    Phongs: [
+      {
+        MaPhong: 'Phong 101',
+        MaNhaId: 'CH-001',
+        TenPhong: 'P.101',
+        Tang: '1',
+        GiaThue: '3000000',
+        DatCoc: '3000000',
+        DienTich: '20',
+        SoKhachToiDa: '3',
+        TrangThai: 'Đang ở'
+      },
+      {
+        MaPhong: 'Phong 201',
+        MaNhaId: 'CH-001',
+        TenPhong: 'P.201',
+        Tang: '2',
+        GiaThue: '2900000',
+        DatCoc: '2900000',
+        DienTich: '20',
+        SoKhachToiDa: '3',
+        TrangThai: 'Đang ở'
+      },
+      {
+        MaPhong: 'Phong 301',
+        MaNhaId: 'CH-001',
+        TenPhong: 'P.301',
+        Tang: '3',
+        GiaThue: '2800000',
+        DatCoc: '2800000',
+        DienTich: '20',
+        SoKhachToiDa: '3',
+        TrangThai: 'Còn trống'
+      }
+    ],
+    PhiDichVus: [
+      {
+        MaDichVu: 'DV-01',
+        MaNhaId: 'CH-001',
+        TenDichVu: 'Điện',
+        LoaiDichVu: 'Tiền điện',
+        DonGia: '2500',
+        DonViTinh: 'đ/Kwh'
+      },
+      {
+        MaDichVu: 'DV-02',
+        MaNhaId: 'CH-001',
+        TenDichVu: 'Nước',
+        LoaiDichVu: 'Tiền nước',
+        DonGia: '9000',
+        DonViTinh: 'm³'
+      },
+      {
+        MaDichVu: 'DV-03',
+        MaNhaId: 'CH-001',
+        TenDichVu: 'Vệ sinh',
+        LoaiDichVu: 'Tiền vệ sinh',
+        DonGia: '30000',
+        DonViTinh: 'đ/người'
+      },
+      {
+        MaDichVu: 'DV-04',
+        MaNhaId: 'CH-001',
+        TenDichVu: 'Internet',
+        LoaiDichVu: 'Tiền vệ sinh',
+        DonGia: '100000',
+        DonViTinh: 'đ/phòng'
+      }
+    ]
+  },
+  {
+    id: 'CH-002',
+    TenNha: 'Hom Tay',
+    DiaChiNha: '100 phố Viên',
+    QuanHuyen: 'Bắc Từ Liêm',
+    TinhThanh: 'Hà Nội',
+    XaPhuong: 'Cổ Nhuế 2',
+    TrangThai: 'Hoạt động',
+    Phongs: [
+      {
+        MaPhong: 'Phong 101',
+        MaNhaId: 'CH-002',
+        TenPhong: 'P.101',
+        Tang: '1',
+        GiaThue: '3000000',
+        DatCoc: '3000000',
+        DienTich: '20',
+        SoKhachToiDa: '3',
+        TrangThai: 'Đang ở'
+      },
+      {
+        MaPhong: 'Phong 201',
+        MaNhaId: 'CH-002',
+        TenPhong: 'P.201',
+        Tang: '2',
+        GiaThue: '2900000',
+        DatCoc: '2900000',
+        DienTich: '20',
+        SoKhachToiDa: '3',
+        TrangThai: 'Đang ở'
+      },
+      {
+        MaPhong: 'Phong 301',
+        MaNhaId: 'CH-002',
+        TenPhong: 'P.301',
+        Tang: '3',
+        GiaThue: '2800000',
+        DatCoc: '2800000',
+        DienTich: '20',
+        SoKhachToiDa: '3',
+        TrangThai: 'Còn trống'
+      }
+    ],
+    PhiDichVus: [
+      {
+        MaDichVu: 'DV-01',
+        MaNhaId: 'CH-002',
+        TenDichVu: 'Điện',
+        LoaiDichVu: 'Tiền điện',
+        DonGia: '2500',
+        DonViTinh: 'đ/Kwh'
+      },
+      {
+        MaDichVu: 'DV-02',
+        MaNhaId: 'CH-002',
+        TenDichVu: 'Nước',
+        LoaiDichVu: 'Tiền nước',
+        DonGia: '9000',
+        DonViTinh: 'm³'
+      },
+      {
+        MaDichVu: 'DV-03',
+        MaNhaId: 'CH-002',
+        TenDichVu: 'Vệ sinh',
+        LoaiDichVu: 'Tiền vệ sinh',
+        DonGia: '30000',
+        DonViTinh: 'đ/người'
+      },
+      {
+        MaDichVu: 'DV-04',
+        MaNhaId: 'CH-002',
+        TenDichVu: 'Internet',
+        LoaiDichVu: 'Tiền vệ sinh',
+        DonGia: '100000',
+        DonViTinh: 'đ/phòng'
+      }
+    ]
+  }
+]
